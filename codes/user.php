3fb18411cee357379c775281b9ed3dea72c36423
foadelamoury <foad.osama@hotmail.com>
2019-02-14 15:22:14 +0200

    Update user.php

--- codes/user.php
+++ codes/user.php
@@ -1,3 +1,31 @@
+<?php
+echo'
+<html>
+	<body>
+		<form method="POST" action="#">
+			<h3>Add user</h3>
+			<input type="text" name="name" placeholder="Enter username here">
+			<input type="submit" name="add" value="insert">
+		</form>	
+		
+		<form method="POST" action="#">
+			<h3>Delete user</h3>
+			<input type="text" name="userName" placeholder="Enter username here">
+			<input type="submit" name="deleteAccount" value="delete">
+		</form>	
+		
+		<form method="POST" action="#">
+			<h3>Update user</h3>
+			<input type="text" name="userName" placeholder="Enter username here">
+			<input type="text" name="updatedName" placeholder="Enter updated Name">
+			<input type="submit" name="updateAccount" value="update">
+		</form>	
+		
+	</body>
+</html>';
+///////////////////////////////////////////
+$string1 ='
+
 <html>
 	<body>
 		<form method="POST" action="#">
@@ -21,8 +49,9 @@
 		
 	</body>
 </html>
-
-
+';
+ var_dump($string1);
+?>
 <?php
 
 	$conn = new mysqli("localhost","root","","crudtask");
