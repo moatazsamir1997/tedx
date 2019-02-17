78acab1afc3a6ca78e8a46195bb6921a4e098b59
foadelamoury <foad.osama@hotmail.com>
2019-02-17 22:02:35 +0200

    Revert "Update user.php"
    This reverts commit 3fb18411cee357379c775281b9ed3dea72c36423.

--- codes/user.php
+++ codes/user.php
@@ -1,31 +1,3 @@
-<?php
-echo'
-<html>
-	<body>
-		<form method="POST" action="#">
-			<h3>Add user</h3>
-			<input type="text" name="name" placeholder="Enter username here">
-			<input type="submit" name="add" value="insert">
-		</form>	
-		
-		<form method="POST" action="#">
-			<h3>Delete user</h3>
-			<input type="text" name="userName" placeholder="Enter username here">
-			<input type="submit" name="deleteAccount" value="delete">
-		</form>	
-		
-		<form method="POST" action="#">
-			<h3>Update user</h3>
-			<input type="text" name="userName" placeholder="Enter username here">
-			<input type="text" name="updatedName" placeholder="Enter updated Name">
-			<input type="submit" name="updateAccount" value="update">
-		</form>	
-		
-	</body>
-</html>';
-///////////////////////////////////////////
-$string1 ='
-
 <html>
 	<body>
 		<form method="POST" action="#">
@@ -49,9 +21,8 @@ $string1 ='
 		
 	</body>
 </html>
-';
- var_dump($string1);
-?>
+
+
 <?php
 
 	$conn = new mysqli("localhost","root","","crudtask");
