120e0169348feac25aa864cefdfdda4ee9546f07
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-02-22 19:48:19 +0200

    sql file

--- crudtask.sql
+++ crudtask.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2019 at 04:47 PM
+-- Generation Time: Feb 22, 2019 at 06:47 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.4
 
@@ -25,13 +25,104 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `parentId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attending`
+--
+
+CREATE TABLE `attending` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `eventId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board`
+--
+
+CREATE TABLE `board` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `openingDate` date NOT NULL,
+  `closingDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boarddetails`
+--
+
+CREATE TABLE `boarddetails` (
+  `id` int(11) NOT NULL,
+  `departmentId` int(11) NOT NULL,
+  `boardId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `jobDescribtion` text NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `eventStart` time NOT NULL,
+  `eventEnd` time NOT NULL,
+  `addressId` int(11) NOT NULL,
+  `GPSLocation` text NOT NULL,
+  `boardId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+
+CREATE TABLE `gender` (
+  `id` int(1) NOT NULL,
+  `name` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `links`
 --
 
 CREATE TABLE `links` (
   `id` int(11) NOT NULL,
   `physicalName` varchar(255) NOT NULL,
-  `friendlyName` varchar(255) NOT NULL
+  `friendlyName` varchar(255) NOT NULL,
+  `htmlCode` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -43,26 +134,10 @@ CREATE TABLE `links` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `userTypeId` int(11) NOT NULL
+  `userTypeId` int(11) NOT NULL DEFAULT '1',
+  `userAddressId` int(11) NOT NULL,
+  `genderId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `userTypeId`) VALUES
-(1, 'mohamed', 1),
-(2, 'mohamed', 1),
-(3, 'mohamed', 1),
-(4, 'mohamed', 1),
-(5, 'mohamed', 1),
-(6, 'mohamed', 1),
-(7, 'mohamed', 1),
-(8, 'mohamed', 1),
-(9, 'mohamed', 1),
-(10, 'mohamed', 1),
-(11, 'mohamed', 1),
-(12, 'mohamed', 1);
 
 -- --------------------------------------------------------
 
@@ -72,15 +147,9 @@ INSERT INTO `user` (`id`, `name`, `userTypeId`) VALUES
 
 CREATE TABLE `usertype` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `parentId` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `usertype`
---
-
-INSERT INTO `usertype` (`id`, `name`) VALUES
-(1, 'employee');
 
 -- --------------------------------------------------------
 
@@ -99,6 +168,56 @@ CREATE TABLE `usertypelinks` (
 --
 
 --
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parentId` (`parentId`);
+
+--
+-- Indexes for table `attending`
+--
+ALTER TABLE `attending`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`,`eventId`),
+  ADD KEY `eventId` (`eventId`);
+
+--
+-- Indexes for table `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `boarddetails`
+--
+ALTER TABLE `boarddetails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `departmentId` (`departmentId`),
+  ADD KEY `boardId` (`boardId`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `addressId` (`addressId`),
+  ADD KEY `boardId` (`boardId`);
+
+--
+-- Indexes for table `gender`
+--
+ALTER TABLE `gender`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `links`
 --
 ALTER TABLE `links`
@@ -109,13 +228,17 @@ ALTER TABLE `links`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `userTypeId` (`userTypeId`);
+  ADD KEY `userTypeId` (`userTypeId`),
+  ADD KEY `userAddressId` (`userAddressId`),
+  ADD KEY `id` (`id`),
+  ADD KEY `genderId` (`genderId`);
 
 --
 -- Indexes for table `usertype`
 --
 ALTER TABLE `usertype`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parentId` (`parentId`);
 
 --
 -- Indexes for table `usertypelinks`
@@ -130,6 +253,42 @@ ALTER TABLE `usertypelinks`
 --
 
 --
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attending`
+--
+ALTER TABLE `attending`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `board`
+--
+ALTER TABLE `board`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `boarddetails`
+--
+ALTER TABLE `boarddetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gender`
+--
+ALTER TABLE `gender`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `links`
 --
 ALTER TABLE `links`
@@ -139,13 +298,13 @@ ALTER TABLE `links`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usertype`
 --
 ALTER TABLE `usertype`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usertypelinks`
@@ -158,17 +317,58 @@ ALTER TABLE `usertypelinks`
 --
 
 --
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`parentId`) REFERENCES `address` (`id`);
+
+--
+-- Constraints for table `attending`
+--
+ALTER TABLE `attending`
+  ADD CONSTRAINT `attending_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `attending_ibfk_2` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`);
+
+--
+-- Constraints for table `boarddetails`
+--
+ALTER TABLE `boarddetails`
+  ADD CONSTRAINT `boarddetails_ibfk_1` FOREIGN KEY (`boardId`) REFERENCES `board` (`id`),
+  ADD CONSTRAINT `boarddetails_ibfk_2` FOREIGN KEY (`departmentId`) REFERENCES `department` (`id`);
+
+--
+-- Constraints for table `department`
+--
+ALTER TABLE `department`
+  ADD CONSTRAINT `department_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `event`
+--
+ALTER TABLE `event`
+  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`addressId`) REFERENCES `address` (`id`),
+  ADD CONSTRAINT `event_ibfk_2` FOREIGN KEY (`boardId`) REFERENCES `board` (`id`);
+
+--
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`);
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`userAddressId`) REFERENCES `address` (`id`),
+  ADD CONSTRAINT `user_ibfk_4` FOREIGN KEY (`genderId`) REFERENCES `gender` (`id`),
+  ADD CONSTRAINT `user_ibfk_5` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`);
+
+--
+-- Constraints for table `usertype`
+--
+ALTER TABLE `usertype`
+  ADD CONSTRAINT `usertype_ibfk_1` FOREIGN KEY (`parentId`) REFERENCES `usertype` (`id`);
 
 --
 -- Constraints for table `usertypelinks`
 --
 ALTER TABLE `usertypelinks`
-  ADD CONSTRAINT `usertypelinks_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`),
-  ADD CONSTRAINT `usertypelinks_ibfk_2` FOREIGN KEY (`linkId`) REFERENCES `links` (`id`);
+  ADD CONSTRAINT `usertypelinks_ibfk_2` FOREIGN KEY (`linkId`) REFERENCES `links` (`id`),
+  ADD CONSTRAINT `usertypelinks_ibfk_3` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
