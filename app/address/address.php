6d65d7cc3e234d07cf1e9c0dbc99ac18f917e7e8
foadelamoury <foad.osama@hotmail.com>
2019-02-27 19:01:32 +0200

    address

---
+++ app/address/address.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$con = new mysqli("localhost", "root", "","web-ex");
+
+
+}
+class Address
+{
+
+  public function getAddress($addressId)
+  {
+     if ($addressId!=0) {
+
+       $sql="select * from address where `id`= '".$addressId."'  ";
+       $result = mysqli_query($con,$sql);
+       if(mysqli_num_rows($result) > 0)
+       {
+
+       	while($row = mysqli_fetch_array($result))
+       	{
+
+       	 $addressId=$row['parentId'];
+
+
+
+        }
+
+getAddress($addressId);
+     }
+  }
+
+}
+
+
+
+
+
+ ?>
+ <form  method="post">
+
+
+
+
+
+ </form>
