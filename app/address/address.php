2d9e35bbea40127d2cf2a3c7dfe2bb18ad23fe88
foadelamoury <foad.osama@hotmail.com>
2019-02-27 22:49:23 +0200

    update

--- app/address/address.php
+++ app/address/address.php
@@ -1,36 +1,34 @@
 <?php
 
-
-$con = new mysqli("localhost", "root", "","crudtask");
-
-
-
-class Address
+/**
+ *
+ */
+ include_once ("conn.php");
+class address
 {
+  public $id;
+  public $fullname;
+  public $email;
+  public $dob;
+  public $password;
+  public $usertype_id;
 
-  public function getAddress($addressId)
+  function insert($address)
   {
-     if ($addressId!=0) {
+    $sql = "INSERT INTO Tutorial2 (ID, FullName, Email, DOB, Password, UserType_Id)
+    VALUES ('".$user->id."', 'amr', 'amr@gmail.com' , '7-7-97' , '123', '1')";
 
-       $sql="select * from address where `id`= '".$addressId."'  ";
-       $result = mysqli_query($con,$sql);
-       if(mysqli_num_rows($result) > 0)
-       {
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-       	while($row = mysqli_fetch_array($result))
-       	{
+    $conn->close();
 
-       	 $addressId=$row['parentId'];
-
-
-
-        }
-
-getAddress($addressId);
-     }
   }
 
-}
+
 }
 
 
@@ -38,7 +36,7 @@ getAddress($addressId);
 
 
  ?>
- <form  method="post">
+ <form  method="post" action=>
 
 
         <h3>Add address</h3>
@@ -46,14 +44,31 @@ getAddress($addressId);
         <select>
   <option value="egypt">Egypt</option>
 </select> 
-<select>
-  <option value="cairo">Cairo</option>
-  <option value="alexandria">Alexandria</option>
-</select> 
+<select name="city">
+    <option value="10">Cairo</option>
+    <option value="11">Alexandria</option>
+</select>
 
-      <input type="text" placeholder="enter street name">
-
+      <input type="text" placeholder="enter street name" name="streetName">
+      <input type="submit" class="Submit_Button" name="submit">
 </form>
-        
+
+
+       <?php
+      
+
+
+$sql = "INSERT INTO address (`name`, `parentId`)
+VALUES ('".$_POST['streetName']."', '".$_POST['city']."')";
+
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+
+mysqli_close($con);
+
+       ?> 
 
 
