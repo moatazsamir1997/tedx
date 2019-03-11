f183a212e09e6182b88a2319c0fae658f0667610
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-03-12 00:05:38 +0200

    wss

---
+++ backend/purchasing/Manufacture.php
@@ -0,0 +1,18 @@
+<?php
+class Manufacture extends Model implements Icrud
+{
+    private $addressId
+    private $contactNumberId
+
+    public function __construct()
+    {
+        $this->tableName = 'manufacture';
+        $this->culomnNameArr = array('name' , 'addressId', 'contactNumberId')
+    }
+
+    public function store($request){}
+    public function update($request){}
+    public function delete($request){}
+    public function search($request){}    
+    
+}
