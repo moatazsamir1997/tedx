887b8f7b62e2721f5e8c99d22b8f7af1baa4c95e
foadelamoury <foad.osama@hotmail.com>
2019-03-16 01:11:08 +0200

    adding no editing

---
+++ backend/Event.php
@@ -0,0 +1,27 @@
+<?php
+include('product\DBHellper.php')
+class Event extends DBHelper implements Icrud
+{  
+    private $date;
+    private $eventStart;
+    private $eventEnd;
+    private $addressId;
+    private $academicYearId;
+    private $userId;
+   
+
+
+    public function __construct()
+    {
+        $this->tableName = 'event';
+        $this->culomnNameArr = array('date', 'eventStart', 'eventEnd', 'addressId', 'academicYearId');
+    } 
+    public function store($request){}
+    public function update($request){}
+    public function delete($request){}
+    public function search($request){}
+    
+
+    
+        
+}
---
+++ backend/links/Links.php
@@ -0,0 +1,45 @@
+<?php
+require_once('backend\product\DBHelper.php');
+class Links extends DBHelper implements Icrud
+{
+    private $physicalName;        
+    private $friendlyName;
+    private $htmlCode;
+    private $userTypeId;
+
+   
+
+
+    public function __construct()
+    {
+        $this->tableName = 'Links';
+        $this->columnNamesArr = array('physicalName','friendlyName','htmlCode');
+    }
+
+    public function store($request)
+    {
+        // $this->name = $request['name'];
+        // $this->price = $request['price'];
+        // $this->quantity = $request['quantity'];
+        // $this->productTypeId = $request['productTypeId'];
+		// $this->columnValuesArr = array( $this->name, $this->price, $this->quantity, $this->productTypeId); 		
+		// $this->insert($this->columnNamesArr , $this->columnValuesArr , $this->tableName);
+    }
+
+    public function update($request)
+    {
+        // $valueArr = array('ss', 50, 100, 44);
+        // $this->dynamicUpdate($this->tableName, $this->columnNamesArr, $valueArr , $where = 'id = 1' );
+    }
+
+    public function delete($request)
+    {
+        # code...
+    }
+    public function search($request)
+    {
+        # code...
+    }
+
+    
+}
