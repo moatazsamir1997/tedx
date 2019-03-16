fbf3fdb1ef0a6e43b1b810bc86e79998f5e9088d
AhmedSoufy <AhmedSoufy@outlook.com>
2019-03-16 17:09:20 +0200

    Zahaby Tamplate

---
+++ design/zahaby tamplate/TEDxJMI-master/layouts/headerNavbar.php
@@ -0,0 +1,17 @@
+
+
+<div id="navbar" class="navbar-collapse collapse col-lg-9 pull-right">
+    <ul class="nav navbar-nav font-family-alt letter-spacing-1 text-uppercase font-weight-700">
+        <li><a href="../about/index.php" class="line-height-unset">About</a></li>
+        <li><a href="../speakers/index.php" class="line-height-unset">Speakers</a></li>
+        <li><a href="../our-team/index.php" class="line-height-unset">Our Team</a></li>
+        <li><a href="../alumni/index.php" class="line-height-unset">Alumni</a></li>
+        <li><a href="../sponsors/index.php" class="line-height-unset">Sponsors</a></li>
+        <li><a href="../contact/index.php" class="line-height-unset">Contact Us</a></li>
+        <li class="bg-base-color">
+            <a href="../register/index.php" class="line-height-unset width-100">
+            Register
+            </a>
+        </li>
+    </ul>
+</div>
