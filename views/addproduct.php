eee7ae7af19cde765bccb901edacf8c8708a357f
foadelamoury <foad.osama@hotmail.com>
2019-03-17 21:24:27 +0200

    Update addproduct.php

--- views/addproduct.php
+++ views/addproduct.php
@@ -2,6 +2,15 @@
 <head>
 	<link rel="shortcut icon" href="..\res\images\icons\product.png"/>
 	<link rel="stylesheet" type="text/css" href="..\res\css\addproduct.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body background="..\res\images\addproduct.jpg" >
@@ -32,6 +41,32 @@
 			<button class="submit" type="submit" name="next1" value="submit">next</button>
 			<br><br>
 		</form>
+		<?php   echo '<button type="button"  name="anotherProduct" id="AnotherProduct">add Another Product</button>';
+		
+		echo'<div id="product"></div><br>';
+
+?>
 	</div>
+	<script>
+    
+
+      $(document).ready(function(){
+          $("#AnotherProduct").click(function(){
+
+              $.ajax({
+                  type: 'POST',
+                  url: 'getId.php',
+                  success: function(data) {
+					  
+                      $("#product").html(data);
+		
+				
+                  }
+              });
+     });
+  });
+
+  
+  </script>
 </body>
 
