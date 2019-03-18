e62b9693419ada94f644db5390dcb5c6337ba41a
mohamedalzahaby <mohamedalzahaby@gmail.com>
2019-03-18 18:23:27 +0200

    zahaby

--- views/addproduct.php
+++ views/addproduct.php
@@ -4,9 +4,9 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
 
-	<!-- <link rel="shortcut icon" href="..\res\images\icons\product.png"/> -->
-	<link rel="stylesheet" type="text/css" href="..\res\css\addproduct.css">
-	<!-- <link rel="stylesheet" type="text/css" href="..\res\css\addproduct.css"> -->
+	<!-- <link rel="shortcut icon" href="res\images\icons\product.png"/> -->
+	<link rel="stylesheet" type="text/css" href="res\css\addproduct.css">
+	<!-- <link rel="stylesheet" type="text/css" href="res\css\addproduct.css"> -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
@@ -18,12 +18,12 @@
 
 </head>
 
-<body background="..\res\images\addproduct.jpg" >
+<body background="res\images\addproduct.jpg" >
 
 	<div class="addproduct">
 		<h1><b>Add Product</b></h1>
 		<br><br><hr><br>	
-		<form action = '..\web\product.php' method ='POST'>
+		<form action = 'web\product.php' method ='POST'>
 			<p><b>product name </b></p>
 			<input type="text" name="productname" placeholder="Enter product name here" required>
 			<br><br>
